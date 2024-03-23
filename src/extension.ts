d36d2a14dbb55777dcc8182be9b7c1f3d644d0f8
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2024-03-23 11:32:04 -0300

    Naming adjustment

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,10 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Congratulations, your extension "mauiformatting" is now active!');
+	console.log('Congratulations, your extension "zoioDeLula" is now active!');
 
-	let disposable = vscode.commands.registerCommand('mauiformatting.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from mauiformatting!');
+	let disposable = vscode.commands.registerCommand('zoioDeLula.helloWorld', () => {
+		vscode.window.showInformationMessage('Hello World from zoioDeLula!');
 	});
 
 	context.subscriptions.push(disposable);
