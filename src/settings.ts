c39baeae6547265ffad78a21a15eb0c4a875b5c6
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2024-03-23 13:36:51 -0300

    Adding the Settings

---
+++ src/settings.ts
@@ -0,0 +1,30 @@
+export interface ISettings {
+    attributesInNewlineThreshold: number;
+    formatOnSave: boolean;
+    positionAllAttributesOnFirstLine: boolean;
+    useSelfClosingTags: boolean;
+}
+
+export const defaultSettings: ISettings = {
+    attributesInNewlineThreshold: 1,
+    formatOnSave: false,
+    positionAllAttributesOnFirstLine: false,
+    useSelfClosingTags: true
+};
+
+export class Settings {
+    attributesInNewlineThreshold: number;
+    formatOnSave?: boolean;
+    positionAllAttributesOnFirstLine?: boolean;
+    useSelfClosingTags?: boolean;
+
+    constructor(attributesInNewlineThreshold: number, 
+        formatOnSave?: boolean, 
+        positionAllAttributesOnFirstLine?: boolean, 
+        useSelfClosingTags?: boolean) {
+        this.attributesInNewlineThreshold = attributesInNewlineThreshold ?? defaultSettings.attributesInNewlineThreshold;
+        this.formatOnSave = formatOnSave ?? defaultSettings.formatOnSave;
+        this.positionAllAttributesOnFirstLine = positionAllAttributesOnFirstLine ?? defaultSettings.positionAllAttributesOnFirstLine;
+        this.useSelfClosingTags = useSelfClosingTags ?? defaultSettings.useSelfClosingTags;
+    }
+} 
