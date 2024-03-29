9cf3045278ced8face86a05e20acdee88cad11a6
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2024-03-29 15:00:13 -0300

    First Unit Test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,29 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
+import { XamlFormatter } from "../formatting/xaml-formatter";
+import fs from 'fs';
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('basic formatting', async () => {
+		await testFormatter('basic');
 	});
 });
+
+async function testFormatter(fileName: string): Promise<void> {
+	let xamlFormatter = new XamlFormatter();
+
+	const expectedFormattedXaml = dataLoader(`${fileName}.formatted.xaml`).replace(/\r/g, "");
+	const unformattedXaml = dataLoader(`${fileName}.unformatted.xaml`);
+
+	const document = await vscode.workspace.openTextDocument({ content: unformattedXaml });
+	const textEdits = xamlFormatter.formatXaml(document);
+
+	const actualFormattedText = textEdits.map(edit => edit.newText).join('');
+	assert.strictEqual(actualFormattedText, expectedFormattedXaml, 'The current formatted XAML does not match the expected formatted XAML.');
+}
+
+function dataLoader(fileName: string): string {
+	return fs.readFileSync(`${__dirname}/../../src/test/data/${fileName}`, 'utf-8');
+}
