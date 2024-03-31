e881a928788c949a0c567a12ebcc013ffd6da106
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2024-03-31 15:00:30 -0300

    Update settings.ts

--- src/common/settings.ts
+++ src/common/settings.ts
@@ -1,34 +1,34 @@
 export interface ISettings {
     attributesInNewlineThreshold: number;
-    formatOnSave: boolean;
     positionAllAttributesOnFirstLine: boolean;
+    putTheFirstAttributeOnTheFirstLine: boolean;
     removeUnusedAttributes: boolean;
     useSelfClosingTags: boolean;
 }
 
 export const defaultSettings: ISettings = {
     attributesInNewlineThreshold: 1,
-    formatOnSave: false,
     positionAllAttributesOnFirstLine: false,
+    putTheFirstAttributeOnTheFirstLine: false,
     removeUnusedAttributes: false,
     useSelfClosingTags: true
 };
 
 export class Settings {
     attributesInNewlineThreshold?: number;
-    formatOnSave?: boolean;
     positionAllAttributesOnFirstLine?: boolean;
+    putTheFirstAttributeOnTheFirstLine?: boolean;
     removeUnusedAttributes?: boolean;
     useSelfClosingTags?: boolean;
 
     constructor(attributesInNewlineThreshold?: number,
-        formatOnSave?: boolean,
         positionAllAttributesOnFirstLine?: boolean,
+        putTheFirstAttributeOnTheFirstLine?: boolean,
         removeUnusedAttributes?: boolean,
         useSelfClosingTags?: boolean) {
         this.attributesInNewlineThreshold = attributesInNewlineThreshold ?? defaultSettings.attributesInNewlineThreshold;
-        this.formatOnSave = formatOnSave ?? defaultSettings.formatOnSave;
         this.positionAllAttributesOnFirstLine = positionAllAttributesOnFirstLine ?? defaultSettings.positionAllAttributesOnFirstLine;
+        this.putTheFirstAttributeOnTheFirstLine = putTheFirstAttributeOnTheFirstLine ?? defaultSettings.putTheFirstAttributeOnTheFirstLine;
         this.removeUnusedAttributes = removeUnusedAttributes ?? defaultSettings.removeUnusedAttributes;
         this.useSelfClosingTags = useSelfClosingTags ?? defaultSettings.useSelfClosingTags;
     }
